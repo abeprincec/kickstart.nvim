35e67279db3866a13da9235513eede75f0da806f
Abraham Prince <abeprincec@gmail.com>
2025-01-05 16:34:46 -0500

    add vimbegood, eslint lsp

--- init.lua
+++ init.lua
@@ -237,7 +237,7 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-
+  'ThePrimeagen/vim-be-good',
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
@@ -725,7 +725,7 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'eslint_lsp', 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
