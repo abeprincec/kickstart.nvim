c346a22ee071dbbcb4fe65a3ec1e5c4cce31ef7b
Abraham Prince <abeprincec@gmail.com>
2025-01-10 09:22:25 -0500

    add harpoon

--- init.lua
+++ init.lua
@@ -929,7 +929,49 @@ require('lazy').setup({
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
+  {
+    'theprimeagen/harpoon',
+    config = function()
+      local harpoon = require 'harpoon'
+      harpoon:setup()
+      vim.keymap.set('n', '<leader>A', function()
+        harpoon:list():prepend()
+      end)
+      vim.keymap.set('n', '<leader>a', function()
+        harpoon:list():add()
+      end)
+      vim.keymap.set('n', '<C-e>', function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end)
 
+      vim.keymap.set('n', '<C-1>', function()
+        harpoon:list():select(1)
+      end)
+      vim.keymap.set('n', '<C-2>', function()
+        harpoon:list():select(2)
+      end)
+      vim.keymap.set('n', '<C-3>', function()
+        harpoon:list():select(3)
+      end)
+      vim.keymap.set('n', '<C-4>', function()
+        harpoon:list():select(4)
+      end)
+      vim.keymap.set('n', '<leader><C-1>', function()
+        harpoon:list():replace_at(1)
+      end)
+      vim.keymap.set('n', '<leader><C-2>', function()
+        harpoon:list():replace_at(2)
+      end)
+      vim.keymap.set('n', '<leader><C-3>', function()
+        harpoon:list():replace_at(3)
+      end)
+      vim.keymap.set('n', '<leader><C-4>', function()
+        harpoon:list():replace_at(4)
+      end)
+    end,
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+  },
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
